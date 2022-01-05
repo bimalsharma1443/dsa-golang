ec1779c21017e87cd5cfa12716402a160968fe0c
Bimal Sharma <bimal.sharma@qlik.com>
2022-01-05 21:37:38 +0530

    fix: add min and max node

--- forthChapter/tree/main.go
+++ forthChapter/tree/main.go
@@ -47,6 +47,83 @@ func insertTreeNode(rootNode, treeNode *TreeNode) {
 
 }
 
+func (binarySearchTree *BinarySearchTree) InOrderTraverseTree(function func(int)) {
+	binarySearchTree.lock.Lock()
+	defer binarySearchTree.lock.Unlock()
+	fmt.Println("------------------------------------------------")
+	inOrderTraverseTree(binarySearchTree.rootNode, function)
+}
+
+func inOrderTraverseTree(treeNode *TreeNode, function func(int)) {
+	if treeNode != nil {
+		inOrderTraverseTree(treeNode.leftNode, function)
+		function(treeNode.value)
+		inOrderTraverseTree(treeNode.rightNode, function)
+	}
+}
+
+func (binarySearchTree *BinarySearchTree) PreOrderTraverseTree(function func(int)) {
+	binarySearchTree.lock.Lock()
+	defer binarySearchTree.lock.Unlock()
+	fmt.Println("------------------------------------------------")
+	preOrderTraverseTree(binarySearchTree.rootNode, function)
+}
+
+func preOrderTraverseTree(treeNode *TreeNode, function func(int)) {
+	if treeNode != nil {
+		function(treeNode.value)
+		preOrderTraverseTree(treeNode.leftNode, function)
+		preOrderTraverseTree(treeNode.rightNode, function)
+	}
+}
+
+func (binarySearchTree *BinarySearchTree) PostOrderTraverseTree(function func(int)) {
+	binarySearchTree.lock.Lock()
+	defer binarySearchTree.lock.Unlock()
+	fmt.Println("------------------------------------------------")
+	postOrderTraverseTree(binarySearchTree.rootNode, function)
+}
+
+func postOrderTraverseTree(treeNode *TreeNode, function func(int)) {
+	if treeNode != nil {
+		postOrderTraverseTree(treeNode.leftNode, function)
+		postOrderTraverseTree(treeNode.rightNode, function)
+		function(treeNode.value)
+	}
+}
+
+func (binarySearchTree *BinarySearchTree) MinNode() *int {
+	binarySearchTree.lock.RLock()
+	defer binarySearchTree.lock.RUnlock()
+	var treeNode *TreeNode
+	treeNode = binarySearchTree.rootNode
+	if treeNode == nil {
+		return (*int)(nil)
+	}
+	for {
+		if treeNode.leftNode == nil {
+			return &treeNode.value
+		}
+		treeNode = treeNode.leftNode
+	}
+}
+
+func (binarySearchTree *BinarySearchTree) MaxNode() *int {
+	binarySearchTree.lock.RLock()
+	defer binarySearchTree.lock.RUnlock()
+	var treeNode *TreeNode
+	treeNode = binarySearchTree.rootNode
+	if treeNode == nil {
+		return (*int)(nil)
+	}
+	for {
+		if treeNode.rightNode == nil {
+			return &treeNode.value
+		}
+		treeNode = treeNode.rightNode
+	}
+}
+
 func (binarySearchTree *BinarySearchTree) String() {
 	binarySearchTree.lock.Lock()
 	defer binarySearchTree.lock.Unlock()
@@ -78,4 +155,9 @@ func main() {
 	binarySearchTree.InsertElement(1, 1)
 	binarySearchTree.InsertElement(6, 6)
 	binarySearchTree.String()
+	binarySearchTree.InOrderTraverseTree(func(i int) { fmt.Println(i) })
+	binarySearchTree.PreOrderTraverseTree(func(i int) { fmt.Println(i) })
+	binarySearchTree.PostOrderTraverseTree(func(i int) { fmt.Println(i) })
+	fmt.Println("Min node is : ", *binarySearchTree.MinNode())
+	fmt.Println("Max node is : ", *binarySearchTree.MaxNode())
 }
